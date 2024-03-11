2429f261d77544022898efc972882387d38141cf
eric <eric.requena.7e7@itb.cat>
2024-03-11 12:01:21 +0100

    Add project files.

---
+++ Ejercicio 15 UF5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio_15_UF5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce tu correo electrónico: ");
+            string correo = Console.ReadLine();
+            if (correo.Contains("@") && correo.Contains("."))
+            {
+                Console.WriteLine("El correo es válido");
+            }
+            else
+            {
+                Console.WriteLine("El correo no es válido");
+            }
+        }
+    }
+}
